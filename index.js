66413c758de825e375727853e72ec76c44e23c20
RobertJanksGit <67938304+RobertJanksGit@users.noreply.github.com>
2020-07-28 20:15:24 -0500

    finished task 6 and 7

--- index.js
+++ index.js
@@ -125,14 +125,16 @@ Your function should accept:
 2 arguments 1 for your new array and one for your original array
 
 and should return a new array that is identical to the old array. You can name the new array however you'd like. */
-
-function copy(/*code here*/){
-
-    /*code here*/
-
+let copyOfOriginal;
+function copy(newArray, originalArray){
+    newArray = [... originalArray];
+    return newArray;
 }
-
-/* Task 7: July 7th is "World Chocolate Day" and Baskin Robins wants to create promotional materials highlighting all of their chocolate flavors. Write a function that checks every item in the array for a given string and returns a new array called filteredArray with just these values. Rather than hardcoding "chocolate" into your function, pass a string as a parameter, and invoke with the argument "chocolate". This way you could also filter for "Vanilla", "Sherbert", etc. when those holidays roll around.
+console.log(copy(copyOfOriginal, originalFlavors));
+/* Task 7: July 7th is "World Chocolate Day" and Baskin Robins wants to create promotional materials highlighting all of their chocolate flavors. 
+Write a function that checks every item in the array for a given string and returns a new array called filteredArray with just these values. 
+Rather than hardcoding "chocolate" into your function, pass a string as a parameter, and invoke with the argument "chocolate". This way you could 
+also filter for "Vanilla", "Sherbert", etc. when those holidays roll around.
 
 Your function should accept: 
 
@@ -147,12 +149,17 @@ DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem.
 
 hint - you can use the .includes method to help you solve this */
 
-function filterByWord(/*code here*/){
+function filterByWord(anArray, aString){
+    let filteredArray = [];
 
-    /*code here*/
-
+    for (let i = 0; i < anArray.length; i++) {
+        if (anArray[i].includes(aString)) {
+            filteredArray.push(anArray[i]);
+        }
+    }
+    return filteredArray;
 }
-
+console.log(filterByWord(originalFlavors, "Chocolate"));
 
 
 /* 🧁🍦🍨 STRETCH 🍨🍦🍫*/ 
